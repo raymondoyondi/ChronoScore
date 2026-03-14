b9b6e84ed9f30207a78e21ac9325089379ce16bb
Raymond Oyondi <raymondoyondi@gmail.com>
2026-03-13 21:13:33 -0500

    Create RealTimeLeaderboardApplicationTests.java

---
+++ src/test/java/com/leaderboard /RealTimeLeaderboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leaderboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeLeaderboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

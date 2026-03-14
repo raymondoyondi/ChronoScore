e3423e48cad7c6b9be7b5250d822b1d0fd1553be
Raymond Oyondi <raymondoyondi@gmail.com>
2026-03-13 23:46:21 -0500

    Create UserScore.java

---
+++ src/main/java/com/leaderboard /entity/UserScore.java
@@ -0,0 +1,28 @@
+package com.leaderboard.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user_score")
+public class UserScore {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long userId;
+    private int score;
+    private LocalDateTime createdAt;
+}
